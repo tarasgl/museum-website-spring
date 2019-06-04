1c093d3942bdcc0f4a80898f0108588e05ae0c2c
tarasgl <tarasgl@gmail.com>
2019-06-04 20:26:00 +0300

    added javadoc to Exhibit controller

--- src/main/java/com/softserve/academy/museum/controler/ExhibitController.java
+++ src/main/java/com/softserve/academy/museum/controler/ExhibitController.java
@@ -9,6 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+/**
+ * Controller class for exhibit page
+ */
 @Controller
 public class ExhibitController {
     @Autowired
@@ -29,6 +33,14 @@ public class ExhibitController {
     @Autowired
     private EmployeeService employeeService;
 
+
+    /**
+     * Handles get request for exhibit page
+     * sets all attributes needed for exhibit page and forwards request
+     * to museum-website.exhibits tile
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits")
     public String exhibits(Model model){
         model.addAttribute("exhibits", exhibitService.getAll());
@@ -42,6 +54,16 @@ public class ExhibitController {
         return "museum-website.exhibits";
     }
 
+
+    /**
+     * Handles get request for exhibits filtered by author
+     * sets exhibits attribute with list of exhibits and forwards request
+     * to exhibits.jsp
+     * If id is null returns list of all exhibits
+     * @param id - Integer, id of author;
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits/filterByAuthor")
     public String exhibitsByAuthor(@RequestParam(name="authorId") Integer id, Model model){
         if (id == null){
@@ -51,7 +73,15 @@ public class ExhibitController {
         }
         return "exhibits";
     }
-
+    /**
+     * Handles get request for exhibits filtered by hall
+     * sets exhibits attribute with list of exhibits and forwards request
+     * to exhibits.jsp
+     * If id is null returns list of all exhibits
+     * @param id - Integer, id of hall;
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits/filterByHall")
     public String exhibitsByHall(@RequestParam(name="hallId") Integer id, Model model){
         if (id == null){
@@ -62,9 +92,17 @@ public class ExhibitController {
         return "exhibits";
     }
 
+    /**
+     * Handles get request for exhibits filtered by material
+     * sets exhibits attribute with list of exhibits and forwards request
+     * to exhibits.jsp
+     * @param material - string, material name
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits/filterByMaterial")
     public String exhibitsByMaterial(@RequestParam(name="material") String material, Model model){
-        if (material == null || material.equals("")){
+        if (material == null || material.isEmpty()){
             model.addAttribute("exhibits", exhibitService.getAll());
         } else {
             model.addAttribute("exhibits", exhibitService.getByMaterial(material));
@@ -72,9 +110,17 @@ public class ExhibitController {
         return "exhibits";
     }
 
+    /**
+     * Handles get request for exhibits filtered by technique
+     * sets exhibits attribute with list of exhibits and forwards request
+     * to exhibits.jsp
+     * @param technique - string, technique name
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits/filterByTechnique")
     public String exhibitsByTechnique(@RequestParam(name="technique") String technique, Model model){
-        if (technique == null || technique.equals("")){
+        if (technique == null || technique.isEmpty()){
             model.addAttribute("exhibits", exhibitService.getAll());
         } else {
             model.addAttribute("exhibits", exhibitService.getByTechnique(technique));
@@ -82,6 +128,15 @@ public class ExhibitController {
         return "exhibits";
     }
 
+    /**
+     * Handles get request for exhibits filtered by employee
+     * sets exhibits attribute with list of exhibits and forwards request
+     * to exhibits.jsp
+     * If employeeId is null returns list of all exhibits
+     * @param employeeId - Integer, id of hall;
+     * @param model
+     * @return
+     */
     @GetMapping("/exhibits/filterByEmployee")
     public String exhibitsByEmployeeId(@RequestParam(name="employeeId") Integer employeeId, Model model){
         if (employeeId == null){
